91e0895c6385f4cfe9fd69f47f574ca7eaba58d0
ls1w <michelle_j.b@hotmail.com>
2023-10-26 09:50:11 -0300

    conversor funcional

---
+++ conversor.js
@@ -0,0 +1,37 @@
+let temperatura;
+let Celsius;
+let Fahrenheit;
+let escala = "";
+let resultado;
+
+function conversor(){
+
+    //entrada do conversor seleionado
+    escala = document.querySelector('#slcConversor').value;
+
+    //entrada da temperatura digitada
+    temperatura = document.querySelector('#temperatura').value;
+    
+    //verifica se é o conversor é Celsius
+    if (escala == 'Celsius') {
+
+        //CALCULO DE CONVERSÃO
+        Celsius = (temperatura - 32) / 1.8;
+
+        //EXIBIR O VALOR CONVERTIDO
+        resultado = Celsius;
+        document.querySelector('#resultado').innerHTML = resultado;
+    }
+
+    //verifica se é o conversor é Fahrenheit
+    if (escala == 'Fahrenheit') {
+
+        //CALCULO DE CONVERSÃO
+        Fahrenheit = temperatura * 1.8 + 32;
+
+        //EXIBIR O VALOR CONVERTIDO
+        resultado = Fahrenheit;
+        document.querySelector('#resultado').innerHTML = resultado;
+    }
+
+}
